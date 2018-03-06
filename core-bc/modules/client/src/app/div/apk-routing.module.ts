f886a7fd6e3f4c7bf5ac3e1b3a3f49fe9df7a082
mrg3kko <erik.andersson@monator.com>
2018-03-06 15:54:18 +0100

    Added back button to add req page

--- core-bc/modules/client/src/app/div/apk-routing.module.ts
+++ core-bc/modules/client/src/app/div/apk-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
       {
         path: ':id/add-requisition',
         component: PatientAddRequisitionComponent
-        canDeactivate: [FormChangedGuard]
+        //canDeactivate: [FormChangedGuard]
       },
 
       {
