3be841af5a8899f9956f3a4ec619d5a98abaa185
mrg3kko <erik.andersson@monator.com>
2018-04-18 16:12:05 +0200

    Added example of print on beställnings page

--- core-bc/modules/client/src/app/div/prescription-detail/prescription-detail.component.ts
+++ core-bc/modules/client/src/app/div/prescription-detail/prescription-detail.component.ts
@@ -37,4 +37,57 @@ export class PrescriptionDetailComponent implements OnInit {
       this.bestallningsRader = data;
     });
   }
+
+  print(): void {
+      let printContents, popupWin;
+      printContents = document.getElementById('bastallningsTableWrap').innerHTML;
+      popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+      popupWin.document.open();
+      popupWin.document.write(`
+        <html>
+          <head>
+            <title>Beställning</title>
+            <style>
+              .mat-table {
+                  display: block;
+              }
+
+              .mat-header-row,
+              .mat-row {
+                  align-items: center;
+                  border-bottom: 1px solid #333;
+                  box-sizing: border-box;
+                  display: flex;
+                  padding: 0 24px;
+              }
+
+              .mat-header-cell {
+                  color: #555;
+                  font-size: 12px;
+                  font-weight: bold;
+              }
+
+              .mat-cell,
+              .mat-header-cell {
+                  flex: 1;
+                  overflow: hidden;
+                  padding: 10px;
+                  word-wrap: break-word;
+              }
+
+              .mat-table .mat-cell {
+              	 font-size: 14px;
+                 overflow: visible;
+                 color: #333;
+              }
+
+            </style>
+          </head>
+      <body onload="window.print();window.close()">${printContents}</body>
+        </html>`
+      );
+      popupWin.document.close();
+  }
+
+
 }
